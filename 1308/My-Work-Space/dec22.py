88842f90cfad569d9dabe6e8fe67af6a86fed08b
Satyampz <112633266+Satyampz@users.noreply.github.com>
2026-01-26 16:45:14 +0530

    Add files via upload

---
+++ 1308/My-Work-Space/dec22.py
@@ -0,0 +1,20 @@
+# Bytes and bytearrey
+
+a=bytes()
+# print(a,type(a))
+
+a=bytes("hello","utf-8")
+# print(a,type(a))
+
+b1=bytes([65,66,67,68,69])
+# print(b1,type(b1))
+
+# print(b1[0])
+
+ab=bytes("Python","utf-8")
+# print(ab,type(ab))
+
+# ab.append(65)
+# print(ab)
+
+# print(print(print("Python")))
